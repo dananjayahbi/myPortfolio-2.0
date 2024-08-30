be8b81410ae1823fab2c0ddaf311aab093fe981c
djd <dananjayahbi@gmail.com>
2024-08-30 13:13:00 +0530

    feat: Add new pages for Home, AdminLogin, SiteSettings, and AdminDashboard
    This commit adds new pages for Home, AdminLogin, SiteSettings, and AdminDashboard. Each page is a functional component that renders a simple div with the respective page name. These pages will be used for the frontend of the application.

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,9 +1,49 @@
-import React from 'react'
+import React from "react";
+import { Routes, Route, Navigate } from "react-router-dom";
+import Home from "./pages/Home";
+import AdminDashboard from "./pages/adminPages/adminDashboard";
+import AdminLogin from "./pages/adminPages/adminLogin";
+import SiteSettings from "./pages/adminPages/SiteSettings";
+import "./App.css";
+
+// Custom PrivateRoute component
+const PrivateRoute = ({ children }) => {
+  const isLogged = window.sessionStorage.getItem("LoggedIn");
+
+  return isLogged === true ? children : <Navigate to="/admin/login" />;
+};
 
 const App = () => {
   return (
-    <div>App</div>
-  )
-}
+    <>
+      <Routes>
+        <Route path="/" element={<Home />} />
 
-export default App
+        {/* Protecting /admin routes */}
+        <Route
+          path="/admin"
+          element={
+            <PrivateRoute>
+              <AdminDashboard />
+            </PrivateRoute>
+          }
+        />
+
+        {/* Protecting /admin/siteSettings route */}
+        <Route
+          path="/admin/siteSettings"
+          element={
+            <PrivateRoute>
+              <SiteSettings />
+            </PrivateRoute>
+          }
+        />
+
+        {/* Admin login route */}
+        <Route path="/admin/login" element={<AdminLogin />} />
+      </Routes>
+    </>
+  );
+};
+
+export default App;
